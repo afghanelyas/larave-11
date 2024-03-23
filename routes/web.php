730e02b7a5297b53ab49994497458d2a490c4086
afghanelyas <afghan.elyas1@gmail.com>
2024-03-23 14:01:14 +0430

    update the route

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,56 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'PHP Developer',
+                'salary' => '$3,000',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Python Developer',
+                'salary' => '$3,500',
+            ],
+            [
+                'id' => 3,
+                'title' => 'Java Developer',
+                'salary' => '$4,000',
+            ],
+        ],
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'PHP Developer',
+            'salary' => '$3,000',
+        ],
+        [
+            'id' => 2,
+            'title' => 'Python Developer',
+            'salary' => '$3,500',
+        ],
+        [
+            'id' => 3,
+            'title' => 'Java Developer',
+            'salary' => '$4,000',
+        ],
+    ];
+
+    $job = Arr::first($jobs, fn ($job) => $job['id'] == $id);
+
+    return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
