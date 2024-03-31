45b38effa1a3b2c0e2d11e510681ad9c940e47f9
afghanelyas <afghan.elyas1@gmail.com>
2024-03-31 04:36:28 +0430

    update the job listing table

--- database/migrations/2024_03_29_010237_create_job_listing_table.php
+++ database/migrations/2024_03_29_010237_create_job_listing_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('job_listing', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('employer_id');
             $table->string('title');
             $table->string('salary');
             $table->timestamps();
