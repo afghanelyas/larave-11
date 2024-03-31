8a02127fc40364892c83f8f15c3c5396ec1d1cb2
afghanelyas <afghan.elyas1@gmail.com>
2024-03-31 04:35:44 +0430

    create job model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,38 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'PHP Developer',
-                'salary' => '$3,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Python Developer',
-                'salary' => '$3,500',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Java Developer',
-                'salary' => '$4,000',
-            ],
-        ];
-    }
+    use HasFactory;
 
-    public static function find(int $id): ?array
-    {
-        $job = Arr::first(static::all(), fn ($job) => $job['id'] == $id);
-        if (! $job) {
-            abort(404);
-        }
+    protected $table = 'job_listing';
 
-        return $job;
-    }
+    protected $guarded = [];
 }
